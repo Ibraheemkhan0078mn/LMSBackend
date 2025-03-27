bf0400171ac931be7526efac443d0d8b331e9927
ibrahimKhan <ikkhan007m@gmail.com>
2025-03-27 05:47:58 +0500

    db setup

--- db/dbConnection.js
+++ db/dbConnection.js
@@ -1,10 +1,12 @@
 import mongoose from 'mongoose'
+import dotenv from "dotenv"
+dotenv.config()
 
 
 
 async function dbConnection (){
     try{
-        mongoose.connect("mongodb://localhost:27017/LMS")
+        mongoose.connect(process.env.mongodbUri)
         .then((response)=>{
             console.log("Mongodb is connected")
         })
